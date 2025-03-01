3e1a77c53546b306ee3af0e4348cf15b768b262d
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2025-03-01 17:56:23 +0100

    updated teacher page with session data

--- app/dashboard/teacher/page.tsx
+++ app/dashboard/teacher/page.tsx
@@ -1,14 +1,17 @@
 // import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { auth } from "@/app/(auth)/auth";
 import { Button } from "@/components/ui/button";
-import { Calendar, Clock, Users, Wallet } from "lucide-react";
 
-export default function TeacherDashboard() {
+export default async function TeacherDashboard() {
+  const session = await auth();
   return (
     <div className="p-6 space-y-6">
       {/* Header */}
       <div className="flex justify-between items-center">
         <div>
-          <h1 className="text-2xl font-bold">Welcome back, Sarah</h1>
+          <h1 className="text-2xl font-bold">
+            Welcome back, {session?.user?.email}
+          </h1>
           <p className="text-muted-foreground">
             Here's what's happening with your classes today.
           </p>
